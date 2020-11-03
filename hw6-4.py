cee53b5e645ff7e83a71ebb725601fa6c8bc4815
Alex Joliet <p21ajoliet@fairfieldprep.org>
2020-11-03 10:02:08 -0500

    HW edits

--- hw6-4.py
+++ hw6-4.py
@@ -13,9 +13,9 @@ calendar.TextCalendar(6).pryear(2020)
 print(calendar.month(2020, 9))
 
 #Question 4
-calendar.LocaleTextCalendar(6, "SPANISH").pryear(2020)
-calendar.LocaleTextCalendar(6, "ENGLISH").pryear(2020)
-calendar.LocaleTextCalendar(6, "MARATHI").pryear(2020)
+#calendar.LocaleTextCalendar(6, "SPANISH").pryear(2020)
+#calendar.LocaleTextCalendar(6, "ENGLISH").pryear(2020)
+#calendar.LocaleTextCalendar(6, "MARATHI").pryear(2020)
 ##calendar.LocaleTextCalendar(6, "PIG LATIN").pryear(2020)
 
 #Question 5
